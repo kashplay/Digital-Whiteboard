fc465095d78a13ad403987e47205b481ba0b9d32
kashish <kashishsingh8700@gmail.com>
2020-09-01 02:20:01 +0530

    new Files

---
+++ public/js/zoomin.js
@@ -0,0 +1,13 @@
+const zoomin=document.querySelector(".zoom-in");
+const zoomout=document.querySelector(".zoom-out");
+
+zoomin.addEventListener("click",function(e){
+
+    ctx.scale(1.1,1.1);
+    // ctx.translate(-50,-0);
+})
+zoomout.addEventListener("click",function(){
+    
+    ctx.scale(0.95,0.95);
+    // ctx.translate(30,20);
+})
